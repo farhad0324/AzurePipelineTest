80468688d612d4c0ad21c87a3f033aaf8d780051
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-06-30 21:11:21 +0500

    Update main.tf

--- AzurePipeline/main.tf
+++ AzurePipeline/main.tf
@@ -2,7 +2,12 @@ provider "azurerm" {
   features {}
 }
 
+# Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "rg" {
-  name     = "MyResourceGroup1"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.resource_group_location
+
+  tags = {
+    environment = "farhad"
+  }
 }

13c45d3f38d7623f1d3af0233ce3d19e5a1cbe24
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-07-01 18:36:28 +0500

    Update main.tf

--- Azurepipeline/main.tf
+++ Azurepipeline/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
-
+}
   tags = {
     environment = "farhad"
   }

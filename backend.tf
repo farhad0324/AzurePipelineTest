37cc1e24a57dc4c308968cce21abe06164b34b17
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-07-02 19:50:01 +0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  // backend "azurerm" {
-  //   resource_group_name  = "Terraform"
-  //   storage_account_name = "terraformteststorage"
-  //   container_name       = "terraformtestcontainer"
-  //   key                  = "terraform.tfstate"
-  // }
-}

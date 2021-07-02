76297bfb5582b97ebda5dfbceb1f6587d646b330
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-07-02 19:37:55 +0500

    Add files via upload

---
+++ Azurepipeline/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  // backend "azurerm" {
+  //   resource_group_name  = "Terraform"
+  //   storage_account_name = "terraformteststorage"
+  //   container_name       = "terraformtestcontainer"
+  //   key                  = "terraform.tfstate"
+  // }
+}

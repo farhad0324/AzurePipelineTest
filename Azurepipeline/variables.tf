5f324188e51d6a97073eeb0602e8b022a00458d5
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-07-07 18:43:55 +0500

    Add files via upload

---
+++ Azurepipeline/variables.tf
@@ -0,0 +1,54 @@
+data "azurerm_key_vault_secret" "resourcegroupname" {
+  name         = "resourcegroupname"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "resourcegrouplocation" {
+  name         = "resourcegrouplocation"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "tags" {
+  name         = "tags"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "tagenv" {
+  name         = "tagenv"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "tagpur" {
+  name         = "tagpur"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "virtualnetworkname" {
+  name         = "virtualnetworkname"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "subnetname" {
+  name         = "subnetname"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "publicipname" {
+  name         = "publicipname"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "networksecuritygroupname" {
+  name         = "networksecuritygroupname"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "networkinterfacename" {
+  name         = "networkinterfacename"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}
+
+data "azurerm_key_vault_secret" "linuxvirtualmachinename" {
+  name         = "linuxvirtualmachinename"
+  key_vault_id = "/subscriptions/9b6de23d-d9d6-472e-879a-259432656f29/resourceGroups/Terraform/providers/Microsoft.KeyVault/vaults/terraformkeyvault2"
+}

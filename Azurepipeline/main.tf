510a1bfd9cb7fbd57d9a8ce374913602626e88c6
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-07-07 23:04:27 +0500

    Update main.tf

--- Azurepipeline/main.tf
+++ Azurepipeline/main.tf
@@ -95,6 +95,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  
+   security_rule {
+    name                       = "HTTP"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
   tags = {
     Creater = data.azurerm_key_vault_secret.tags.value

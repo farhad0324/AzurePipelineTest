b659bac35db84c11cc01aaa8c003b95ae4ac120b
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-07-13 18:27:58 +0500

    Update Main.tf

--- Azure Single VM/Main.tf
+++ Azure Single VM/Main.tf
@@ -4,52 +4,61 @@ provider "azurerm" {
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name     = data.azurerm_key_vault_secret.resourcegroupname.value
+  location = data.azurerm_key_vault_secret.resourcegrouplocation.value
 
   tags = {
-    Name = var.tag_name
-    Owner = var.tag_owner
+    Owner = data.azurerm_key_vault_secret.tags.value
+    Environment = data.azurerm_key_vault_secret.tagenv.value
+    Purpose = data.azurerm_key_vault_secret.tagpur.value
   }
 }
 
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
+  name                = data.azurerm_key_vault_secret.virtualnetworkname.value
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  depends_on = [azurerm_resource_group.rg]
 
   tags = {
-    Name = var.tag_name
+    Owner = data.azurerm_key_vault_secret.tags.value
+    Environment = data.azurerm_key_vault_secret.tagenv.value
+    Purpose = data.azurerm_key_vault_secret.tagpur.value
   }
 }
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                 = data.azurerm_key_vault_secret.subnetname.value
+  resource_group_name  = data.azurerm_key_vault_secret.resourcegroupname.value
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [azurerm_resource_group.rg]
 }
 
 # Create public IPs
 resource "azurerm_public_ip" "public_ip" {
-  name                = var.public_ip_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = data.azurerm_key_vault_secret.publicipname.value
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
   allocation_method   = "Dynamic"
+  depends_on = [azurerm_resource_group.rg]
 
   tags = {
-    Name = var.tag_name
+    Owner = data.azurerm_key_vault_secret.tags.value
+    Environment = data.azurerm_key_vault_secret.tagenv.value
+    Purpose = data.azurerm_key_vault_secret.tagpur.value
   }
 }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "nsg" {
-  name                = var.network_security_group_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = data.azurerm_key_vault_secret.networksecuritygroupname.value
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
+  depends_on = [azurerm_resource_group.rg]
 
   security_rule {
     name                       = "SSH"
@@ -76,25 +85,31 @@ resource "azurerm_network_security_group" "nsg" {
   }
   
   tags = {
-    Name = var.tag_name
+    Owner = data.azurerm_key_vault_secret.tags.value
+    Environment = data.azurerm_key_vault_secret.tagenv.value
+    Purpose = data.azurerm_key_vault_secret.tagpur.value
   }
 }
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                = var.network_interface_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = data.azurerm_key_vault_secret.networkinterfacename.value
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
+  depends_on = [azurerm_resource_group.rg]
 
   ip_configuration {
-    name                          = "myNicConfiguration"
+    name                          = "MyNicConfiguration"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 
+
   tags = {
-    Name = var.tag_name
+    Owner = data.azurerm_key_vault_secret.tags.value
+    Environment = data.azurerm_key_vault_secret.tagenv.value
+    Purpose = data.azurerm_key_vault_secret.tagpur.value
   }
 }
 
@@ -104,29 +119,6 @@ resource "azurerm_network_interface_security_group_association" "association" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "storage" {
-  name                     = "diag${random_id.randomId.hex}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    Name = var.tag_name
-  }
-}
-
 # Create (and display) an SSH key
 resource "tls_private_key" "azurevm_ssh" {
   algorithm = "RSA"
@@ -134,12 +126,12 @@ resource "tls_private_key" "azurevm_ssh" {
 }
 
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "linuxvm" {
-  name                  = var.linux_virtual_machine_name
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+name                  = data.azurerm_key_vault_secret.linuxvirtualmachinename.value
+  location              = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name   = data.azurerm_key_vault_secret.resourcegroupname.value
   network_interface_ids = [azurerm_network_interface.nic.id]
   size                  = "Standard_DS1_v2"
+  depends_on = [azurerm_resource_group.rg]
 
   os_disk {
     name                 = "myOsDisk"
@@ -163,11 +155,9 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
     public_key = tls_private_key.azurevm_ssh.public_key_openssh
   }
 
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
-  }
-
   tags = {
-    Name = var.tag_name
+    Owner = data.azurerm_key_vault_secret.tags.value
+    Environment = data.azurerm_key_vault_secret.tagenv.value
+    Purpose = data.azurerm_key_vault_secret.tagpur.value
   }
 }

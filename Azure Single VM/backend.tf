c018bc3af445546cbfcc7a297c876bdeffe29f2d
farhad0324 <86664777+farhad0324@users.noreply.github.com>
2021-09-23 18:48:21 +0500

    Update backend.tf

--- Azure Single VM/backend.tf
+++ Azure Single VM/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "Terraform"
     storage_account_name = "terraformteststorage"
     container_name       = "terraformtestcontainer"
-    key                  = "terraform.tfstate"
+  #  key                  = "terraform.tfstate"
   }
 }
